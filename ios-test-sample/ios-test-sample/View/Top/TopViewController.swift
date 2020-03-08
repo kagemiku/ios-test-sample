7d8028ed7998d2c278a2de5ace92bf5a79282e9a
kagemiku <kagemiku@gmail.com>
2020-03-08 16:22:44 +0900

    Add TopVC

---
+++ ios-test-sample/ios-test-sample/View/Top/TopViewController.swift
@@ -0,0 +1,21 @@
+//
+//  TopViewController.swift
+//  ios-test-sample
+//
+//  Created by kagemiku on 2020/03/08.
+//  Copyright © 2020 kagemiku. All rights reserved.
+//
+
+import UIKit
+
+class TopViewController: UIViewController {
+
+    var id: String = ""
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        title = id
+    }
+
+}
