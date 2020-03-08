7fd477891ef473780ff8dc5b4982e151840d74de
kagemiku <kagemiku@gmail.com>
2020-03-08 15:46:30 +0900

    Add xcodeproj

---
+++ ios-test-sample/ios-test-sampleTests/ios_test_sampleTests.swift
@@ -0,0 +1,34 @@
+//
+//  ios_test_sampleTests.swift
+//  ios-test-sampleTests
+//
+//  Created by kagemiku on 2020/03/08.
+//  Copyright © 2020 kagemiku. All rights reserved.
+//
+
+import XCTest
+@testable import ios_test_sample
+
+class ios_test_sampleTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
