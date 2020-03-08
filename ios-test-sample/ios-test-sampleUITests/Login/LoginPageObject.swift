432ed5761670db4d3f1362dbb3a31ec34b9d1162
kagemiku <kagemiku@gmail.com>
2020-03-08 21:45:18 +0900

    Implement LoginPageObject

--- ios-test-sample/ios-test-sampleUITests/Login/LoginPageObject.swift
+++ ios-test-sample/ios-test-sampleUITests/Login/LoginPageObject.swift
@@ -6,4 +6,45 @@
 //  Copyright © 2020 kagemiku. All rights reserved.
 //
 
-import Foundation
+import XCTest
+
+final class LoginPageObject: PageObjectProtocol {
+
+    private let app: XCUIApplication
+
+    private var view: XCUIElement {
+        app.otherElements[AccesibilityIdentifier.loginView.rawValue]
+    }
+
+    private var idTextField: XCUIElement {
+        view.textFields[AccesibilityIdentifier.loginIDTextField.rawValue]
+    }
+
+    private var passwordTextField: XCUIElement {
+        view.secureTextFields[AccesibilityIdentifier.loginPasswordTextField.rawValue]
+    }
+
+    private var loginButton: XCUIElement {
+        view.buttons[AccesibilityIdentifier.loginLoginButton.rawValue]
+    }
+
+    var pageExists: Bool {
+        view.exists
+    }
+
+    init(application: XCUIApplication) {
+        self.app = application
+    }
+
+    func login() -> Void {
+        XCTContext.runActivity(named: "Login") { _ in
+            idTextField.typeText("kagemiku")
+            passwordTextField.typeText("password")
+
+            loginButton.tap()
+        }
+
+        return ()
+    }
+
+}
