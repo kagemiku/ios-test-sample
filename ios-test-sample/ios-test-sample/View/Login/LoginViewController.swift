80288bd123aeb7ab04525ee654f2205669d89a50
kagemiku <kagemiku@gmail.com>
2020-03-08 16:06:44 +0900

    Add LoginVC

---
+++ ios-test-sample/ios-test-sample/View/Login/LoginViewController.swift
@@ -0,0 +1,22 @@
+//
+//  LoginViewController.swift
+//  ios-test-sample
+//
+//  Created by kagemiku on 2020/03/08.
+//  Copyright © 2020 kagemiku. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    // MARK: IBOutlets
+
+    @IBOutlet private weak var idTextField: UITextField!
+    @IBOutlet private weak var passwordTextField: UITextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
